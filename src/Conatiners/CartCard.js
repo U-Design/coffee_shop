9f4ea7d987b47744cdea1da75779e4b3c10db045
skishore <samir.kishore@nokia.com>
2018-09-24 13:49:53 +0530

    First Commit

--- src/Conatiners/CartCard.js
+++ src/Conatiners/CartCard.js
@@ -24,7 +24,7 @@ class CartCoffee extends React.Component{
                         </div>
                         <div className='col-sm-4'>
                             <span>Quantity : {coffee.qty}</span>
-                            
+
                         </div>
                     </div>
 
